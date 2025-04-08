a8893f8462c0f8e7783b5af905d6928fd9de223a
Javiton2005 <javierantonordonez@gmail.com>
2025-04-08 12:50:33 +0200

    init

---
+++ server/server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/members")
+def members():
+    return {"members": ["mem1","mem2","mem3","mem4"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
