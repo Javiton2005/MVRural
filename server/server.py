cf84ab6aafffa045ae5a84702f82f997bf04adc8
Javiton2005 <javierantonordonez@gmail.com>
2025-04-29 20:04:28 +0200

    La puta base de datos esta creada

--- server/server.py
+++ server/server.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
 
 app = Flask(__name__)
 
-@app.route("/members")
-def members():
-    return {"members": ["mem1",{"saludos":["hello","goodby","adios"]},"mem3","mem4"]}
+@app.route("/pueblos", methods=["GET"])
+def get_pueblos():
+    n = request.args.get("n", default=5, type=int)  # Por defecto devuelve 5
+    response = supabase.table("pueblos").select("*").limit(n).execute()
+    return jsonify(response.data)
 
 if __name__ == "__main__":
     app.run(debug=True)
